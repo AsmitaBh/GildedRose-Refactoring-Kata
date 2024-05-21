a11b5386a0165ce85a6fa693f1fb0aeb7333ebfb
asmita bhattacharya <asmita.bhattacharya@bnpparibasfortis.com>
2024-05-22 01:11:23 +0200

    Refactored tests: extracted method to update quality for multiple days and reused

--- Java-Approvals/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java-Approvals/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -5,10 +5,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
+
+    private GildedRose gildedRose;
+
     @Test
     void testNormalItem_qualityDecreasesEachDay() {
         Item item = new Item("+5 Dexterity Vest", 10, 20);
-        GildedRose gildedRose = new GildedRose(new Item[] {item});
+        gildedRose = new GildedRose(new Item[]{item});
         gildedRose.updateQuality();
         assertEquals(9, item.sellIn);
         assertEquals(19, item.quality);
@@ -17,11 +20,8 @@ class GildedRoseTest {
     @Test
     void testNormalItem_whenSellByDatePassed_qualityDecreasesTwiceAsFastEachDay() {
         Item item = new Item("+5 Dexterity Vest", 10, 20);
-        GildedRose gildedRose = new GildedRose(new Item[] {item});
-
-        for (int dayIndex = item.sellIn; dayIndex > -1; dayIndex--) {
-            gildedRose.updateQuality();
-        }
+        gildedRose = new GildedRose(new Item[]{item});
+        updateQualityForMultipleDays(item.sellIn);
         assertEquals(-1, item.sellIn);
         assertEquals(8, item.quality);
     }
@@ -29,12 +29,16 @@ class GildedRoseTest {
     @Test
     void testNormalItem_qualityNeverLessThanZero() {
         Item item = new Item("+5 Dexterity Vest", 10, 20);
-        GildedRose gildedRose = new GildedRose(new Item[] {item});
+        gildedRose = new GildedRose(new Item[]{item});
 
-        for (int dayIndex = 21; dayIndex > -1; dayIndex--) {
-            gildedRose.updateQuality();
-        }
+        updateQualityForMultipleDays(21);
         assertEquals(-12, item.sellIn);
         assertEquals(0, item.quality);
+    }
+
+    private void updateQualityForMultipleDays(int days) {
+        for (int dayIndex = days; dayIndex > -1; dayIndex--) {
+            gildedRose.updateQuality();
+        }
     }
 }
