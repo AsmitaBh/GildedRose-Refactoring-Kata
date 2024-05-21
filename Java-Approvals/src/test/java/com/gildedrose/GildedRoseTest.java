4cc37cc410bfd3b174d0a5946504b9d32d8d3cbd
asmita bhattacharya <asmita.bhattacharya@bnpparibasfortis.com>
2024-05-22 00:31:47 +0200

    Added unit test case: for Normal item when Sell by date is passed quality degrades twice as fast

--- Java-Approvals/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java-Approvals/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -13,4 +13,16 @@ class GildedRoseTest {
         assertEquals(9, item.sellIn);
         assertEquals(19, item.quality);
     }
+
+    @Test
+    void testNormalItem_whenSellByDatePassed_qualityDecreasesTwiceAsFastEachDay() {
+        Item item = new Item("+5 Dexterity Vest", 10, 20);
+        GildedRose gildedRose = new GildedRose(new Item[] {item});
+
+        for (int dayIndex = item.sellIn; dayIndex > -1; dayIndex--) {
+            gildedRose.updateQuality();
+        }
+        assertEquals(-1, item.sellIn);
+        assertEquals(8, item.quality);
+    }
 }
