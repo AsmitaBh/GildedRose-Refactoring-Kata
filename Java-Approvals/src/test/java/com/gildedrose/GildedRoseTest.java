b2ea857caf787ecf6423921866842a824f0deb71
asmita bhattacharya <asmita.bhattacharya@bnpparibasfortis.com>
2024-05-22 11:25:09 +0200

    Added unit test case: for Backstage Pass Item, when sellIn is 10 days or less, then quality increases by 2 each day

--- Java-Approvals/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java-Approvals/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -89,6 +89,15 @@ class GildedRoseTest {
         assertEquals(21, item.quality);
     }
 
+    @Test
+    void testBackstagePass_whenSellInIsTenOrLess_qualityIncreasesByTwoEachDay() {
+        Item item = getBackstagePassItem();
+        gildedRose = new GildedRose(new Item[]{item});
+        updateQualityForMultipleDays(10);
+        assertEquals(9, item.sellIn);
+        assertEquals(32, item.quality);
+    }
+
     private void updateQualityForMultipleDays(int days) {
         for (int dayIndex = days; dayIndex > -1; dayIndex--) {
             gildedRose.updateQuality();
@@ -106,6 +115,7 @@ class GildedRoseTest {
     private static Item getSulfurasItem() {
         return new Item("Sulfuras, Hand of Ragnaros", 10, 80);
     }
+
     private static Item getBackstagePassItem() {
         return new Item("Backstage passes to a TAFKAL80ETC concert", 20, 20);
     }
